f8e7900dbe452f40ff235eb20f6980a83c13e578
Christopher <chrisobrien416@gmail.com>
2020-11-11 18:42:18 -0500

    first commit

---
+++ src/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    PORT: process.env.PORT || 8000,
+    NODE_ENV: process.env.NODE_ENV || 'development',
+    DATABASE_URL: `postgres://xtottmugrfawpy:eaaf9f2f3002a79ac4d362b4042c3e43524a07740df78124c sslmode=require"  277dfa2ea17271a740df78124c@ec2-100-25-100-81.compute-1.amazonaws.com:5432/dvh84sqkjue4o`
+}
---
+++ src/server.js
@@ -0,0 +1,16 @@
+const path = require('path')
+const knex = require('knex')
+const app = require('./app')
+
+const { PORT, DATABASE_URL } = require('./config')
+
+const db = knex({
+    client: 'pg',
+    connection: DATABASE_URL
+})
+
+app.set('db', db)
+
+app.listen(PORT, () => {
+    console.log(`server listening at http://localhost:${PORT}`)
+})
